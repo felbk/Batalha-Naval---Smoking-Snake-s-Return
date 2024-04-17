05288fec811ac0cf96fa09def115e8ef5df11d74
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-17 00:18:01 -0300

    Pré-nomeação das funções a serem usadas

---
+++ funcoes.py
@@ -0,0 +1,26 @@
+#funções para facilitar a organização
+
+def cria_mapa():
+    return
+    
+def aloca_navio():
+    return
+    
+def aloca_se_possivel():
+    return
+    
+def foi_derrotado():
+    return
+    
+def sorteia_cpu():
+    return
+    
+def valida_entradas():
+    return
+        
+def sorteia_ataque():
+    return
+    
+def registra_ataque():
+    return
+    
