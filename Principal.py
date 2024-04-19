de7f905f4cb663a63ae6d67e8e0b10936f4113bf
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-19 12:15:58 -0300

    Tela de loading

--- Principal.py
+++ Principal.py
@@ -1,8 +1,31 @@
 
 from Basededados import* #Importa variaveis fixas da base de dados
 from funcoes import *
+import time
+play = True
 
-#cabeçalho de inicio
-tituloinicio= "!!!!!Batalha Naval - Smoking Snake's Return!!!!!"
-tituloinicio= "="*len(tituloinicio)+'\n'+tituloinicio + "\n"+'='*len(tituloinicio)
-colorir("green",tituloinicio)
+
+while play:
+    
+    #cabeçalho de inicio
+    tituloinicio= "!!!!!Batalha Naval - Smoking Snake's Return!!!!!"
+    tituloinicio= "="*len(tituloinicio)+'\n'+tituloinicio + "\n"+'='*len(tituloinicio)
+    colorir("green",tituloinicio)
+
+    # Cpu escolhe pais e aloca frotas - A FAZER
+
+
+    # Texto de carregamento
+    print('Iniciando jogo')
+    time.sleep(0.3)
+    print('{} está alocando seus navios para a batalha'.format('Brasil'),end='')
+    time.sleep(0.6)
+    for i in range(2):
+        print('.',end='')
+        time.sleep(0.7)
+    print('.')
+    time.sleep(0.5)
+    colorir('green','!! A BATALHA COMEÇOU !!')
+
+    play = False
+    
