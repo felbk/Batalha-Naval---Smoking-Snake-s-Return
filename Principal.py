1b7c802734d2dd82a26999378410b59ab5b91ebf
Felipe Lambak <Lambakfelipe@gmail.com>
2024-04-19 11:25:51 -0300

    cabeçalho de inicio

--- Principal.py
+++ Principal.py
@@ -2,4 +2,7 @@
 from Basededados import* #Importa variaveis fixas da base de dados
 from funcoes import *
 
-
+#cabeçalho de inicio
+tituloinicio= "!!!!!Batalha Naval - Smoking Snake's Return!!!!!"
+tituloinicio= "="*len(tituloinicio)+'\n'+tituloinicio + "\n"+'='*len(tituloinicio)
+colorir("green",tituloinicio)
